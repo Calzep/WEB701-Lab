a5d597a89028845390dd3b86597cedfce2518cbf
Calzep <caleb-easpn@nmit.live.ac.nz>
2025-07-21 16:35:58 +1200

    login and register routes

--- MERN-Login-Demo/server/index.js
+++ MERN-Login-Demo/server/index.js
@@ -12,19 +12,29 @@ mongoose.connect(mongoUrl, {})
 
 
 app.get('/', async (req, res) => {
-    const user = new UserModel({
-        username: "testuser",
-        password: "password"
-    })
-
-    try {
-        await user.save()
-        res.status(200).send("Create testuser")
-    } catch (error) {
-        console.error(error)
-        res.status(500).send("Failed")
-    }
+    res.status(200).send("Connected to server")
 })
+
+// Register
+app.post('/api/register', async (req, res) => {
+  try {
+    const user = new UserModel(req.body);
+    await user.save();
+    res.json({ message: 'Created new user' });
+  } catch (error) {
+    res.status(400).json({ error: `Unable to create new user: ${error}` });
+  }
+});
+
+// Login
+app.post('/api/login', async (req, res) => {
+  const user = await UserModel.findOne({ username: req.body.username });
+  if (!user || !(await user.comparePassword(req.body.password))) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+  return res.status(200).json({ message: 'Login successful' });
+});
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
